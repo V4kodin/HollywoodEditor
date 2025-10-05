3bdf9439ab26781acc61ddf59308e5fbc3ad8949
Vladislav Bondarenko <bondaren.vlad2014@mail.ru>
2025-10-05 11:19:14 +0300

    /

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,14 +8,15 @@ using System.Windows;
 
 namespace Hollywood_Editor
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
-        private void GitHubLink_Click(object sender, RoutedEventArgs e)
+        protected override void OnStartup(StartupEventArgs e)
         {
-     
-        }   
+            base.OnStartup(e);
+
+            // Создаем и показываем существующий MainWindow
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }
